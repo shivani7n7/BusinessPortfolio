075d89b04f08a70de191ac7bae09ec2f5d51578f
Shivani Mehta <43609999+shivani7n7@users.noreply.github.com>
2019-02-19 23:16:33 +0530

    Add files via upload

---
+++ default.php
@@ -0,0 +1,451 @@
+<!-- <?php
+?> -->
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="fontawesome-free-5.6.3-web/css/all.css">
+  <link rel="stylesheet" href="css/style.css">
+  <title>Home - Website</title>
+</head>
+
+<body>
+
+  <!-- Start Header -->
+
+  <header>
+    <div class="p-1" id="topHeader">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 text-right">
+            <a class="p-1" href="callto:999999999"><i class="fa fa-phone" aria-hidden="true"></i> +(91) 9999999990 </a>
+            <a class="p-1" href="mailto:abc@gmail.com"><i class="fa fa-envelope" aria-hidden="true"></i>
+              +inequiry@gmail.com </a>
+
+          </div>
+        </div>
+      </div>
+    </div>
+    <div id="bottomHeader">
+      <div class="container-fluid">
+        <nav class="navbar navbar-dark navbar-expand-lg">
+          <a class="navbar-brand" href="#">
+            <img src="images/logo_text_white_small.png" alt="" width="250px">
+          </a>
+          <button data-toggle="collapse" data-target="#navbar-Toggler" type="button" class="navbar-toggler">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbar-Toggler">
+            <ul class="navbar-nav">
+              <li class="nav-item active">
+                <a href="default.php" class="nav-link">Home</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a href="#" class="nav-link dropdown-toggle" role="button" data-toggle="dropdown">Services</a>
+                <div class="dropdown-menu">
+                  <a class="dropdown-item" href="WebDesigning.php">
+                    web designing
+                  </a>
+                  <a class="dropdown-item" href="Webdevelopment.php">
+                    web development
+                  </a>
+                  <a class="dropdown-item" href="SEOServices.php">
+                    SEO services
+                  </a>
+                  <a class="dropdown-item" href="SoftwareDev.php">
+                    Software development
+                  </a>
+                  <a class="dropdown-item" href="MobileAppDev.php">
+                    Mobile App Development
+                  </a>
+                  <div class="dropdown-divider"></div>
+                  <a class="dropdown-item" href="GraphicDesign.php">
+                    Graphic Designing
+                  </a>
+                </div>
+              </li>
+              <li class="nav-item">
+                <a href="Portfolio.php" class="nav-link">Portfolio</a>
+              </li>
+              <li class="nav-item">
+                <a href="AboutUs.php" class="nav-link">About Us</a>
+              </li>
+              <li class="nav-item">
+                <a href="ContactUs.php" class="nav-link">Contact Us</a>
+              </li> 
+              </ul>
+              <ul class="navbar-nav flex-row ml-md-auto d-none d-md-flex">
+              <a href="signup.php" class=" d-none d-lg-inline-block mb-3 mb-md-0 ml-md-3">
+                  <button class="btn btn-light btn-sm"><i class="fas fa-user"></i> Sign Up
+                  </button>
+              </a>
+              <a href="login.php" class=" d-none d-lg-inline-block mb-3 mb-md-0 ml-md-3">
+                  <button class="btn btn-light btn-sm"><i class="fas fa-sign-in-alt"></i> Log In
+                  </button>
+              </a>
+
+              </ul>
+          </div>
+        </nav>
+      </div>
+    </div>
+  </header>
+  <!--btn btn-bd-download  d-none d-lg-inline-block mb-3 mb-md-0 ml-md-3 -->
+  <!-- End Header -->
+
+  <!-- Start Home Page top-hero-section  -->
+
+  <section class="top-hero-section p-xs-2 pt-4 pt-5">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-md-5">
+          <h4>
+            Quality-Support-Value
+          </h4>
+          <h1>
+            Best in Class IT Services
+          </h1>
+          <h5>
+            Send us your requirements and we will design everything for you.
+          </h5><br />
+          <button style="background-color: #192A56; color: white;" class="btn btn-lg">
+            Get Started
+          </button>
+          <a href="#">Check Pricing Here</a>
+        </div>
+        <div class="col-md-7">
+          <img class="img-fluid pb-4" src="images/hero-img-half.jpg" alt="">
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Home Page top-hero-section  -->
+
+  <!-- Start Home Page second-section- Our Qualities -->
+  <section class="pt-3 pb-4">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 text-center">
+          <h3>
+            Lots of IT companies in our country. Why should you choose us?
+            <hr />
+          </h3>
+        </div>
+      </div>
+      <div class="row ">
+        <div class="col-md-4 text-center">
+          <img class="img-fluid p-5 img-quality" src="images/our_qualities/customized.png"><br />
+          <h6>
+            Customized Requirements
+          </h6>
+          <p>
+            Every idea is Unique and that's why we craft it as per your requirements.
+          </p>
+        </div>
+        <div class="col-md-4 text-center">
+          <img class="img-fluid p-5 img-quality" src="images/our_qualities/cloud.png"><br />
+          <h6>
+            99% Server Uptime
+          </h6>
+          <p>
+            We make Sure that your product stays up 24*7 with us.
+          </p>
+        </div>
+        <div class="col-md-4 text-center">
+          <img class="img-fluid p-5 img-quality" src="images/our_qualities/support.png"><br />
+          <h6>
+            Customer Support
+          </h6>
+          <p>
+            We understands the sense of urgency when our client needs our help.
+          </p>
+        </div>
+        <div class="row ">
+          <div class="col-md-4 text-center">
+            <img class="img-fluid p-5 img-quality" src="images/our_qualities/highquality.png"><br />
+            <h6>
+              Quality Assurance
+            </h6>
+            <p>
+              Quality is center edifice for us and we make no compromise in it.
+            </p>
+          </div>
+          <div class="col-md-4 text-center">
+            <img class="img-fluid p-5 img-quality" src="images/our_qualities/delivery.png"><br />
+            <h6>
+              On time delivery
+            </h6>
+            <p>
+              We will assure you on time delivery of your product.
+            </p>
+          </div>
+          <div class="col-md-4 text-center">
+            <img class="img-fluid p-5 img-quality" src="images/our_qualities/highvalue.png"><br />
+            <h6>
+              Value of money.
+            </h6>
+            <p>
+              Get the best Service for the money you pay us.<br />
+              We assure to give you value of money.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Home Page second-section-Our Quaalities  -->
+
+  <!-- Start Home Page Req Section  -->
+  <section>
+    <div class="container-fluid req-section">
+      <div class="row align-items-center">
+        <div class="col-md-9 text-center pt-3 pb-2">
+          <h5>
+            Do you have any Requirements ? We can do it for you!
+          </h5>
+        </div>
+        <div class="col-md-3 pt-2 pb-2 text-center ">
+          <button class="btn btn-primary "> Get Started </button>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- End Home Page Req Section  -->
+
+  <!-- Start Home Page about SS Section  -->
+  <div class="container p-3">
+    <div class="row pt-3 pb-3 align-items-center ">
+      <div class="col-md-6" id="aboutSS">
+        <h3>
+          About Simple Snippets
+        </h3>
+        <h6>
+          Simple Snippets is a Tech-Educational channel / platform /
+          Blog /Medium for anyone and everyone,who wants to learn
+          and keep themselves updated with latest information,
+          Technology & computer science trends & information. We
+          are dedicated in providing high quality educational and information content for everyone.
+        </h6>
+        <button class="btn btn-primary">
+          Read More
+        </button>
+      </div>
+      <div class="col-md-6 pt-4 pb-4 p-3">
+        <img class="img-fluid" width=400px src="images/hero-img-half.jpg">
+      </div>
+    </div>
+  </div>
+  <!-- End Home Page about SS Section  -->
+
+  <!-- Start Home Page subscribe Section  -->
+  <section class="subscribe">
+    <div class="row">
+      <div class="col-12 text-center">
+        <h4>
+          Subscribe to our Newsletter
+        </h4>
+        <div class="input-group pl-5 pr-5 pb-2">
+          <input type="email" class="form-control" placeholder="Enter your email">
+          <span class="input-group-btn">
+            <button class="btn btn-theme" type="submit">Subcribe</button>
+          </span>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- End Home Page subscribe Section  -->
+
+
+  <!-- Start Home Page Best class services Section  -->
+  <section class="services pt-2 p-2">
+    <div class="container pt-3">
+      <div class="row">
+        <div class="col-12 text-center">
+          <h4>
+            Our Best in class Services
+          </h4>
+          <hr>
+        </div>
+      </div>
+      <div class="row pt-5">
+        <div class="col-md-4 text-center ">
+          <img class="img-fluid" src="images/Services/web design.png" width=150px>
+          <h5 class="pt-3">
+            Web Designing
+          </h5>
+          <p>
+            We have a talent pool of experienced web designers.
+            <a href="WebDesigning.php" >Read More...</a>
+          </p>
+        </div>
+        <div class="col-md-4 text-center">
+          <img src="images/Services/web dev.png" class="img-fluid" width=150px>
+          <h5 class="pt-3">
+            Web Development
+          </h5>
+          <p>
+            Get Customized website developed in a professional way.
+            <a href="Webdevelopment.php">Read More...</a>
+          </p>
+        </div>
+        <div class="col-md-4 text-center">
+          <img src="images/Services/seo.png" class="img-fluid" width=150px>
+          <h5 class="pt-3">
+            SEO Services
+          </h5>
+          <p>
+            Rank your website number 1 to grab your valuable customer.
+            <a href="SEOServices.php">Read More...</a>
+          </p>
+        </div>
+      </div>
+      <div class="row pt-4 pb-4">
+        <div class="col-md-4 text-center ">
+          <img class="img-fluid" src="images/Services/software dev.png" width=150px>
+          <h5 class="pt-3">
+            Software Development
+          </h5>
+          <p>
+            Customized software solutions according to your needs.
+            <a href="SoftwareDev.php">Read More...</a>
+          </p>
+        </div>
+        <div class="col-md-4 text-center">
+          <img src="images/Services/mobile app.png" class="img-fluid" width=150px>
+          <h5 class="pt-3">
+            Mobile Development
+          </h5>
+          <p>
+            Built Android & iOS Apps for your business to scale your search.
+            <a href="MobileAppDev.php">Read More...</a>
+          </p>
+        </div>
+        <div class="col-md-4 text-center">
+          <img src="images/Services/graphic design.png" class="img-fluid" width=150px>
+          <h5 class="pt-3">
+            Graphic Designing
+          </h5>
+          <p>
+            Get your branding done right with ourprofessional designers.
+            <a href="GraphicDesign.php">Read More...</a>
+          </p>
+        </div>
+      </div>
+    </div>
+
+
+  </section>
+
+  <!-- End Home Page Best class services Section  -->
+
+  <!-- Start Home Page Trusted client Section  -->
+  <section class="clients">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 pt-2 pb-1 text-center">
+          <h4>
+            Some Of our trusted clients.
+          </h4>
+          <hr>
+        </div>
+      </div>
+      <div class="row pb-3 align-items-center">
+        <div class="col-md-2 col-6 text-center">
+          <img src="images/clients/google.png" class="img-fluid p-2">
+        </div>
+        <div class="col-md-2 col-6 text-center">
+          <img src="images/clients/apple-logotype.png"class="img-fluid p-2">
+        </div>
+        <div class="col-md-2 col-6 text-center">
+          <img src="images/clients/amazon.png" class="img-fluid p-2">
+        </div>
+        <div class="col-md-2 col-6 text-center">
+          <img src="images/clients/netflix.png" class="img-fluid p-2">
+        </div>
+        <div class="col-md-2 col-6 text-center">
+          <img src="images/clients/samsung.png" class="img-fluid p-2">
+        </div>
+        <div class="col-md-2 col-6 text-center">
+          <img src="images/clients/linkedin.png" class="img-fluid p-2">
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- End Home Page Trusted client Section  -->
+
+  <!-- Start Footer -->
+
+  <footer class="full-footer">
+    <div class="container top-footer p-md-3 p-1">
+      <div class="row">
+
+        <div class="col-md-3 pl-3 pr-3">
+          <img class="img-fluid" src="images/logo_text_white_small.png" alt="">
+          <p>
+            Simple Snippet is a Tech-Edu Channel/Blog/Platform for anyone and everyone...
+            <a href="#aboutSS">Read More</a>
+          </p>
+          <a style="color:black;" class="p-1" href="https://www.facebook.com/simplesnippets/"><i class="fab fa-2x fa-facebook"></i></a>
+          <a style="color:black;" class="p-1" href="https://plus.google.com/+SimpleSnippets"><i class="fab fa-2x fa-google-plus-square"></i></a>
+          <a style="color:black;" class="p-1" href="https://twitter.com/simplesnippet"><i class="fab fa-2x fa-twitter-square"></i></a>
+          <a style="color:black;" class="p-1" href="https://www.instagram.com/simplesnippets/"><i class="fab fa-2x fa-instagram"></i></a>
+        </div>
+        <div class="col-md-3 pl-3 pr-3">
+          <h3>Important links</h3>
+          <a href="#">Privacy policy</a><br />
+          <a href="https://www.youtube.com/channel/UCRIWTSgd7hGtZhx4RYoASEg">Youtube Channel Link</a><br />
+          <a href="https://simplesnippets.tech/category/latest-news/">Blog Articles</a><br />
+          <a href="#">Social Media</a><br />
+        </div>
+        <div class="col-md-3 pl-3 pr-3">
+          <h3>Our Services</h3>
+          <a href="WebDesigning.php">Web Designing</a><br />
+          <a href="Webdevelopment.php">Web development</a><br />
+          <a href="SEOServices.php">SEO Services</a><br />
+          <a href="SoftwareDev.php">Software Development</a><br />
+          <a href="MobileAppDev.php">Mobile App Development</a><br />
+          <a href="GraphicDesign.php">Graphic Designing</a><br />
+        </div>
+        <div class="col-md-3 pl-3 pr-3">
+          <h3>Contact Us</h3>
+          <a class="p-1" href="callto:999999999"><i class="fa fa-phone" aria-hidden="true"></i> +(91) 9999999990 </a><br />
+          <a class="p-1" href="mailto:abc@gmail.com"><i class="fa fa-envelope" aria-hidden="true"></i>
+            +inequiry@gmail.com </a>
+          <div class="embed-responsive embed-responsive-16by9">
+            <iframe class="embed-responsive-item" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d484236.7195838856!2d73.58344171975712!3d18.524411041566637!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2bf2e67461101%3A0x828d43bf9d9ee343!2sPune%2C+Maharashtra!5e0!3m2!1sen!2sin!4v1548352269089"
+              frameborder="0">
+
+            </iframe>
+          </div>
+
+        </div>
+      </div>
+    </div>
+    <div class="container-fluid bottom-footer pt-1">
+      <div class="row text-center">
+        <div class="col-12">
+          <p>
+            Copyrights &copy; 2018 - All rights reserved
+          </p>
+
+        </div>
+      </div>
+    </div>
+  </footer>
+
+  <!-- End Footer -->
+
+  <script src="bootstrap/JS/jquery-3.3.1.slim.min.js"></script>
+  <script src="bootstrap/JS/popper.min.js"></script>
+  <script src="bootstrap/JS/bootstrap.min.js"></script>
+
+</body>
+
+</html>
