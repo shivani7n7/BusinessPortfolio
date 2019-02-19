8eb852b4029ffd55bb50b15b2017721c2d121295
Shivani Mehta <43609999+shivani7n7@users.noreply.github.com>
2019-02-19 23:14:52 +0530

    Add files via upload

---
+++ Pages./pages/Webdevelopment.php
@@ -0,0 +1,260 @@
+<?php
+?>
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="fontawesome-free-5.6.3-web/css/all.css">
+  <link rel="stylesheet" href="css/style.css">
+  <title>Web Development</title>
+</head>
+
+<body>
+  <script src="bootstrap/JS/jquery-3.3.1.slim.min.js"></script>
+  <script src="bootstrap/JS/popper.min.js"></script>
+  <script src="bootstrap/JS/bootstrap.min.js"></script>
+  <!-- Start Header -->
+  <header>
+    <div class="p-1" id="topHeader">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 text-right">
+            <a class="p-1" href="callto:999999999"><i class="fa fa-phone" aria-hidden="true"></i> +(91) 9999999990 </a>
+            <a class="p-1" href="mailto:abc@gmail.com"><i class="fa fa-envelope" aria-hidden="true"></i>
+              +inequiry@gmail.com </a>
+
+          </div>
+        </div>
+      </div>
+    </div>
+    <div id="bottomHeader">
+      <div class="container-fluid">
+        <nav class="navbar navbar-dark navbar-expand-lg">
+          <a class="navbar-brand" href="#">
+            <img src="images/logo_text_white_small.png" alt="" width="250px">
+          </a>
+          <button data-toggle="collapse" data-target="#navbar-Toggler" type="button" class="navbar-toggler">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbar-Toggler">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a href="default.php" class="nav-link">Home</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a href="#" class="nav-link dropdown-toggle" role="button" data-toggle="dropdown">Services</a>
+                <div class="dropdown-menu">
+                  <a class="dropdown-item active" href="WebDesigning.php">
+                    web designing
+                  </a>
+                  <a class="dropdown-item" href="Webdevelopment.php">
+                    web development
+                  </a>
+                  <a class="dropdown-item" href="SEOServices.php">
+                    SEO services
+                  </a>
+                  <a class="dropdown-item" href="SoftwareDev.php">
+                    Software development
+                  </a>
+                  <a class="dropdown-item" href="MobileAppDev.php">
+                    Mobile App Development
+                  </a>
+                  <div class="dropdown-divider"></div>
+                  <a class="dropdown-item" href="GraphicDesign.php">
+                    Graphic Designing
+                  </a>
+                </div>
+              </li>
+              <li class="nav-item">
+                <a href="Portfolio.php" class="nav-link">Portfolio</a>
+              </li>
+              <li class="nav-item">
+                <a href="AboutUs.php" class="nav-link">About Us</a>
+              </li>
+              <li class="nav-item">
+                <a href="ContactUs.php" class="nav-link">Contact Us</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </div>
+  </header>
+  <!-- End Header -->
+
+  <!-- Start webDev Page breadcrump section -->
+
+  <section class="breadcrumb">
+    <div class="container ">
+      <div class="row">
+        <div class="col-12">
+          <h3>
+            Web Development
+          </h3>
+
+          <ol class="breadcrumb p-0">
+            <li class="breadcrumb-item"><a href="default.php">Home</a></li>
+            <li class="breadcrumb-item">Services</a></li>
+            <li class="breadcrumb-item active">Web Development</li>
+          </ol>
+
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- End webDev Page breadcrump section -->
+
+
+  <!-- Start webDev Page 1st section -->
+  <div class="container">
+    <div class="row">
+      <div class="col-md-9 px-5 p-4 text-justify ">
+        <h5>
+          About Website Development -
+        </h5>
+        <p>
+          Web development is the work involved in developing a web site for the Internet (World Wide Web) or an
+          intranet (a private network).[1] Web development can range from developing a simple single static page of
+          plain text to complex web-based internet applications (web apps), electronic businesses, and social network
+          services. A more comprehensive list of tasks to which web development commonly refers, may include web
+          engineering, web design, web content development, client liaison, client-side/server-side scripting, web
+          server and network security configuration, and e-commerce development.
+        </p>
+        <p>
+          Among web professionals, "web development" usually refers to the main non-design aspects of building web
+          sites: writing markup and coding.[2] Web development may use content management systems (CMS) to make content
+          changes easier and available with basic technical skills.
+        </p>
+        <p>
+          Web development has also impacted personal networking and marketing. Websites are no longer simply tools for
+          work or for commerce, but serve more broadly for communication and social networking. Web sites such as
+          Facebook and Twitter provide users with a platform to communicate and organizations with a more personal and
+          interactive way to engage the public.
+        </p>
+      </div>
+
+      <div class="col-md-3 p-4">
+        <p class="p-4 text-center">
+          <img class="img-fluid" src="images/webDev/web-design.png" width=150>
+        </p>
+        <p class="p-4 text-center">
+          <img class="img-fluid" src="images/webDev/www.png" width=150>
+        </p>
+      </div>
+    </div>
+  </div>
+  <!-- End webDesigning Page 1st section -->
+
+  <!-- Start webDesigning Page skill section -->
+  <section>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <h4>
+            Skill Set-
+          </h4>
+          <div class="bar p-2 m-2" style="background-color: #0c2461; width: 90%;">
+            PHP -90%
+          </div>
+          <div class="bar p-2 m-2" style="background-color: #01a3a4; width: 85%;">
+            MySQL -85%
+          </div>
+          <div class="bar p-2 m-2" style="background-color: #b71540; width: 80%;">
+            jQuery -80%
+          </div>
+          <div class="bar p-2 m-2" style="background-color: #fa983a; width: 95%;">
+            ASP.NET
+          </div>
+        </div>
+
+      </div>
+  </section>
+
+  <!-- End webDesigning Page skill section -->
+
+  <!-- Start webDesign Page Req Section  -->
+  <section>
+    <div class="container-fluid req-section">
+      <div class="row align-items-center">
+        <div class="col-md-9 text-center pt-3 pb-2">
+          <h5>
+            Do you have any Requirements ? We can do it for you!
+          </h5>
+        </div>
+        <div class="col-md-3 pt-2 pb-2 text-center ">
+          <button class="btn btn-primary "> Get Started </button>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- End webDesign Page Req Section  -->
+
+  <!-- Start Footer -->
+  <footer class="full-footer">
+    <div class="container top-footer p-md-3 p-1">
+      <div class="row">
+
+        <div class="col-md-3 pl-3 pr-3">
+          <img class="img-fluid" src="images/logo_text_white_small.png" alt="">
+          <p>
+            Simple Snippet is a Tech-Edu Channel/Blog/Platform for anyone and everyone...
+            <a href="#">Read More</a>
+          </p>
+          <a style="color:black;" class="p-1" href="#"><i class="fab fa-2x fa-facebook"></i></a>
+          <a style="color:black;" class="p-1" href="#"><i class="fab fa-2x fa-google-plus-square"></i></a>
+          <a style="color:black;" class="p-1" href="#"><i class="fab fa-2x fa-twitter-square"></i></a>
+          <a style="color:black;" class="p-1" href="#"><i class="fab fa-2x fa-instagram"></i></a>
+        </div>
+        <div class="col-md-3 pl-3 pr-3">
+          <h3>Important links</h3>
+          <a href="#">Privacy policy</a><br />
+          <a href="#">Youtube Channel Link</a><br />
+          <a href="#">Blog Articles</a><br />
+          <a href="#">Social Media</a><br />
+        </div>
+        <div class="col-md-3 pl-3 pr-3">
+          <h3>Our Services</h3>
+          <a href="WebDesigning.php">Web Designing</a><br />
+          <a href="Webdevelopment.php">Web development</a><br />
+          <a href="SEOServices.php">SEO Services</a><br />
+          <a href="SoftwareDev.php">Software Development</a><br />
+          <a href="MobileAppDev.php">Mobile App Development</a><br />
+          <a href="GraphicDesign.php">Graphic Designing</a><br />
+        </div>
+        <div class="col-md-3 pl-3 pr-3">
+          <h3>Contact Us</h3>
+          <a class="p-1" href="callto:999999999"><i class="fa fa-phone" aria-hidden="true"></i> +(91) 9999999990 </a><br />
+          <a class="p-1" href="mailto:abc@gmail.com"><i class="fa fa-envelope" aria-hidden="true"></i>
+            +inequiry@gmail.com </a>
+          <div class="embed-responsive embed-responsive-16by9">
+            <iframe class="embed-responsive-item" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d484236.7195838856!2d73.58344171975712!3d18.524411041566637!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2bf2e67461101%3A0x828d43bf9d9ee343!2sPune%2C+Maharashtra!5e0!3m2!1sen!2sin!4v1548352269089"
+              frameborder="0">
+
+            </iframe>
+          </div>
+
+        </div>
+      </div>
+    </div>
+    <div class="container-fluid bottom-footer pt-1">
+      <div class="row text-center">
+        <div class="col-12">
+          <p>
+            Copyrights &copy; 2018 - All rights reserved
+          </p>
+
+        </div>
+      </div>
+    </div>
+  </footer>
+  <!-- End Footer -->
+
+</body>
+
+</html>
